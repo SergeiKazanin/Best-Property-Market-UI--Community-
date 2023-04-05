0d7d1fde9296fe19aaacf5ac17b1fbd8b3db4459
SergeiKazanin <serega7667@gmail.com>
2023-04-05 15:57:19 +0700

    Added adaptive

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const loginButton = document.querySelector(".header__login-icon");
 const loginPage = document.querySelector(".login");
 const escapeButton = document.querySelector(".login__escape");
-const loginButton1 = document.querySelector(".header__login-text");
+const loginButton = document.querySelector(".header__login-text");
 
 loginButton.addEventListener("click", () => {
   loginPage.style.transform = "translateX(0)";
 });
-loginButton1.addEventListener("click", () => {
-  loginPage.style.transform = "translateX(0)";
-});
+
 escapeButton.addEventListener("click", () => {
-  loginPage.style.transform = "translateX(-100%)";
+  loginPage.style.transform = "translateX(-220%)";
 });
